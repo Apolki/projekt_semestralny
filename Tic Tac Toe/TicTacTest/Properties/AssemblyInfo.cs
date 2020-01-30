db5218e4ae37afc29487476ab53a9f9d540657b7
Apolki <48468857+Apolki@users.noreply.github.com>
2020-01-30 14:20:01 +0100

    Add files via upload

---
+++ Tic Tac Toe/TicTacTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TicTacTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TicTacTest")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5ee66a34-6dab-448a-bdc4-591a3648db9c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tic Tac Toe/TicTacTest/TicTacTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TicTacTest
+{
+    [TestClass]
+    public class TicTacTest
+    {
+        [TestMethod]
+        public void NewGame()
+        {
+
+            // We tried to do somnething but...
+
+        }
+    }
+}
